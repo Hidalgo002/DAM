bdf13b312f0a01cb2e896f793434050fde84ec60
Hidalgo002 <vhidalgodominguez@gmail.com>
2024-10-23 12:36:55 +0200

    “Trabajo”

---
+++ Programación/Ejercicios_de_Clase/Bucles/For4.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(){
+	int a = 0; //Donde se almacenan los resultados de las multiplicaciones
+	int b = 5; //Valor fijo de 5, es la tabla del 5.
+	int c = 0; //Donde almacenamos los números del 1 al 10
+	
+	for(c=0; c<11; c++){
+	a = c * b;
+	printf("%d x %d = %d\n", c, b, a);}
+
+return 0;
+}
