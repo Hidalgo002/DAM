0f2fb4904d883cefce4967f0aa9c39b6bbe22dba
Hidalgo002 <vhidalgodominguez@gmail.com>
2024-10-23 10:57:25 +0200

    “Trabajo”

---
+++ Programación/Ejercicios_de_Clase/Bucles/For.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(){
+        int i = 0;
+        for (/*Valor inicial*/ i = 0;
+             /*Condición*/ i <= 10;
+             /*Código a ejecutar entre vueltas*/ i++){
+        printf("%d " ,i);}
+
+	printf("Terminado \n");
+
+return 0;
+}
---
+++ Programación/Ejercicios_de_Clase/Bucles/For2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(){
+	int i = 0;
+	for(i=0; i<19; i+=2){
+	printf("%d,", i);}
+	
+	printf("%d. \n", i);
+
+return 0;
+}
