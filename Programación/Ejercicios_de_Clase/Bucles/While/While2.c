788619b5a4c2d86180c706b96660d1d3081261b3
Hidalgo002 <vhidalgodominguez@gmail.com>
2024-10-25 14:28:28 +0200

    “Actualizacion”

---
+++ Programación/Ejercicios_de_Clase/Bucles/While/While2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+void divisor(){
+	for(int i=1000000; i<1; i=i/10);}
+
+int main(){
+	int num = 0;
+	printf("Introduce un número: ");
+	scanf("%d", &num);
+	
+	while(num>0){
+	void divisor;	
+	num=num/divisor;
+	printf("La división entre %d y %d es %d \n", num, divisor, num);
+	}
+
+return 0;
+}
