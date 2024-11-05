94aad6dbd70bbe8283edd6d149c346dbe24eb639
Hidalgo002 <vhidalgodominguez@gmail.com>
2024-11-05 14:29:39 +0100

    “Actualizacion”

---
+++ Programación/Ejercicios_de_Clase/Struct/Struct.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+//DEFINIMOS LOS DATOS DE LOS ESTUDIANTES
+
+struct datos{
+	char nombre[50];
+	int edad;
+	float notas;
+};
+
+
+int main(){
+	struct datos alumno[5]
+	
+	struct datos
+	
+	
+	
+return 0;
+}
+
