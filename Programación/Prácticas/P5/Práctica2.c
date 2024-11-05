96ff24f3c5ca2c18311ef1d2777736454726e003
root <root@DESKTOP-V43T1C9>
2024-11-05 23:48:08 +0100

    Actualización

--- Programación/Prácticas/P5/Práctica2.c
+++ Programación/Prácticas/P5/Práctica2.c
@@ -15,15 +15,15 @@ int main(){
 	}
 
 	//PROMEDIO
-	double promedio = suma / 5;
+	double promedio = suma / 5; //Para el promedio, creo una variable que se llama 'promedio' en la que almaceno la suma de todas las notas y las divido entre 5.
 	printf("Promedio de notas: %.2lf\n", promedio);
 	
 	//NÚMERO DE APROBADOS Y SUSPENSOS
 	int aprobado = 0;
 	int suspenso = 0;
 
-	for(int i = 0; i<5;i++){
-	if(notas[i] >= MÍNIMO){
+	for(int i = 0; i<5;i++){ //El bucle va cambiando la posición del almacenamiento del array
+	if(notas[i] >= MÍNIMO){ //Si la casilla es mayor o igual que el mínimo, entonces, a la varible de aprobado, se le suma 1 para aprobado, si no, se le suma 1 a suspendido
 	aprobado += 1;}
 	else{
 	suspenso += 1;}
@@ -32,13 +32,33 @@ int main(){
 	printf("Número de estudiantes suspensos: %d\n", suspenso);
 
 	//NOTA MÁXIMA Y MÍNIMA:
-	int max = notas[0];
-	for(int i=1;i<5;i++){
-	if(notas[i] > max){
-	max = notas[i];}
+	float max = notas[0]; 
+	int max_posicion = 0; //Igual que antes, para averiguar la posición, se crea una variable independiente para buscar el mayor y almacenarlo en max_posicion
+	float min = notas[0];
+	int min_posicion = 0;
+
+	for(int i=1;i<5;i++){ //int= 1 porque empezamos a comparar el notas[0] con el notas [1]
+		if(notas[i] > max){ //Si el dato del array es mayor al máximo, que inicializa con el valor de la primera casilla, es mayor, etonces, a máximo se le asigna un nuevo valor y se guarda la posicón en max_posicion
+		max = notas[i];
+		max_posicion = i;}
+
+		else{min = notas[i];
+		min_posicion = i;}
 	}
-	printf("Nota máxima:%d\n", max);	
-	
+	printf("Nota máxima: %.2lf (Estudiante en la posición %d)\n", max, max_posicion + 1);
+	printf("Nota mínima: %.2lf (Estudiante en la posición %d)\n", min, min_posicion + 1);
+
+
+	//NOTAS POR ENCIMA DEL PROMEDIO
+	int alto_prom = 0; 
+	for(int i=0;i<5;i++){ 
+                if(notas[i] > promedio){
+		alto_prom = i;	//SI es mayor que el promedio, se guarda el i en alto_prom
+		printf("Estudiante en la posición %d: %.2lf\n", alto_prom + 1, notas[i]);
+		} 
+	}
+
+
 
 return 0;
 }
