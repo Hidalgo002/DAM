15f39dfe5f4ba9586af31e9f9a951a1fdc55d958
Hidalgo002 <vhidalgodominguez@gmail.com>
2024-11-20 14:02:00 +0100

    Huevo

---
+++ Programación/Exámenes/EXEC_Valeria_Hidalgo/EXEC_Valeria_Hidalgo/ejercicio5.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//EJERCICIO 5-> WHILE
+
+int main(){
+	int num;
+	int suma = 0;
+
+        scanf("%d", &num);
+	
+	while('0'<num){
+        suma = num;}
+
+        printf("La suma de los números ingresados es %d. \n", suma);
+	
+
+
+
+
+return 0;
+}
