123f2c4abaab94430d44dc8d7cc4706f49b8911b
Hidalgo002 <vhidalgodominguez@gmail.com>
2025-02-25 12:52:21 +0100

    “Actualizacion”

---
+++ Programación/Prácticas/P8/Valeria/main.c
@@ -0,0 +1,18 @@
+//COMPILAR:  gcc *.c -o programa
+
+
+#include "cazador.h"
+#include "utilidades.h"
+
+int totalCazadores = 3;
+
+int main() {
+
+    BIENVENIDA();
+    
+    MENU();
+
+
+return 0;
+}
+
