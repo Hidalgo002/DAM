2e32328ed435aa7cf1aef35efe6836e4604e042d
Hidalgo002 <vhidalgodominguez@gmail.com>
2025-04-28 12:00:16 +0200

    “Actualizacion”

--- LDM/3.JAVASCRIPT/Java5_Ejercicios.js
+++ LDM/3.JAVASCRIPT/Java5_Ejercicios.js
@@ -1,22 +1,48 @@
-function cuentaAtras(){
+
+//Ejercicio 2
+function cuentaAtras(event){
     let cuenta;
     clearInterval(cuenta);//Inicializamos a cero el contador
 
-    let valorTiempo = document.getElementById("tiempo").value;//Obtenemos el valor puesto en el temporizador
-    let contadorTiempo = document.getElementById("contador");
+    //Evitar la recarga del formulario
+    event.preventDefault(); 
 
-    cuenta= setInterval(x,1000);
+    let valorTiempo = document.getElementById("tiempo").value;//Obtenemos el valor puesto en el temporizador
+    let contador = document.getElementById("contador");
+ 
+    console.log=valorTiempo;
+    cuenta= setInterval(()=>{
+        if(valorTiempo>0){
+            contador.textContent=valorTiempo--;
+        } else {
+            clearInterval(cuenta); //
+            contador.textContent="El tiempo ha expirado."
+        }
+},1000);
 }
 
-/* Crea una tabla en HTML e intenta que dando un botón se rellenen todas las celdas
-con números secuenciales.*/
-function ejercicio3(){
+//Ejercicio 3
+function rellenarTable(){
+    let numero=10
+    for(let i=1;i<=numero;i++){
+        document.getElementById(`${i}`).textContent= i;
+    }
+}
+
+function rellenarTable2(){
+    let celdas = document.querySelectorAll("#tablaSegunda td"); //Seleccionamos todas las celdas (td) de la tabla
+    celdas.forEach((td,i) => {td.textContent=i+1}); //Recorremos todas las celdas
 
 }
 
 /*Cambia el fondo de un div cada vez que el ratón pase por delante del div.*/
-function ejercicio4(){
+function random(number){
+    return Math.floor(Math.random() * (number + 1)); // Genera un número aleatorio entre 0 y el máximo que le pasemos
+}
 
+function cambiarFondo(){
+    let color = `rgb(${random(255)}, ${random(255)}, ${random(255)})`;
+    document.getElementById("fondo").style.backgroundColor = color; // Cambiamos el color de fondo del div
 }
 
 /*Crea un array de palabras, selecciona una palabra aleatoria al cargar la pagina y
@@ -24,7 +50,11 @@ botones con cada una de las letras del abecedario. Cuando se pulse el botón de 
 letra que compruebe si esa letra está y si esta que sume uno a un contador. Esa letra
 no se puede volver a utilizar por lo que hay que desactivar el botón. Si hay dos letras
 en la palabra se cuentan las dos*/
-function ejercicio5(){
+function ahorcado(){
+    let palabras = ["carmelo", "rios", "ferrer", "diaz", "alcalde","grandal"];
+    let palabraSeleccionada = palabras[random(palabras.length)]; // Selecciona una palabra aleatoria del array
+    console.log(event.target.value); // Muestra la palabra seleccionada en la consola para depuración -> podemos elegir si escribir value o textContent, en el primer caso se mostrará el valor del botón (a) y en el segundo el texto que contiene el botón (A)
+
 
 }
 
@@ -40,8 +70,27 @@ function load() {
     /*EJERCICIO 2 --> Crea un reloj que haga una cuenta atrás del número que le hayas insertado a través de un campo de formulario.*/
 
     let botonAtras=document.getElementById("cuentaAtras");
-    botonAtras.addEventListener("submit",cuentaAtras);
+    botonAtras.addEventListener("click",cuentaAtras);
+
+
+    /* EJERCICIO 3 --> Crea una tabla en HTML e intenta que dando un botón se rellenen todas las celdas
+con números secuenciales.*/
+    //opcion 1
+    document.getElementById("table").addEventListener("click",rellenarTable);
+
+    //opcion 2
+    document.getElementById("table2").addEventListener("click",rellenarTable2);
+
     
+    //EJERCICIO 4 --> Cambia el fondo de un div cada vez que el ratón pase por delante del div.
+    document.getElementById("fondo").addEventListener("mouseover", cambiarFondo);
+
+    //EJERCICIO 5 -->
+    let letras = document.querySelectorAll(".ejercicio5"); // Selecciona todos los botones de letras
+    letras.forEach(botonesLetras => botonesLetras.addEventListener("click", ahorcado));
 }
 
 window.addEventListener("DOMContentLoaded", load, false);
+
+
+
